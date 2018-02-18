9922d4dc5795b63293bfccb4174f70e10cf6c737
Ahmad Zainul Ihsan <a.z.ihsan@icloud.com>
2018-02-18 23:26:01 +0100

    Initial files

---
+++ src/base/KomodoApp.C
@@ -0,0 +1,91 @@
+#include "KomodoApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+template <>
+InputParameters
+validParams<KomodoApp>()
+{
+  InputParameters params = validParams<MooseApp>();
+  return params;
+}
+
+KomodoApp::KomodoApp(InputParameters parameters) : MooseApp(parameters)
+{
+  Moose::registerObjects(_factory);
+  ModulesApp::registerObjects(_factory);
+  KomodoApp::registerObjects(_factory);
+
+  Moose::associateSyntax(_syntax, _action_factory);
+  ModulesApp::associateSyntax(_syntax, _action_factory);
+  KomodoApp::associateSyntax(_syntax, _action_factory);
+
+  Moose::registerExecFlags(_factory);
+  ModulesApp::registerExecFlags(_factory);
+  KomodoApp::registerExecFlags(_factory);
+}
+
+KomodoApp::~KomodoApp() {}
+
+void
+KomodoApp::registerApps()
+{
+  registerApp(KomodoApp);
+}
+
+void
+KomodoApp::registerObjects(Factory & /*factory*/)
+{
+  /* Uncomment Factory parameter and register your new production objects here! */
+}
+
+void
+KomodoApp::associateSyntax(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+{
+  /* Uncomment Syntax and ActionFactory parameters and register your new production objects here! */
+}
+
+void
+KomodoApp::registerObjectDepends(Factory & /*factory*/)
+{
+}
+
+void
+KomodoApp::associateSyntaxDepends(Syntax & /*syntax*/, ActionFactory & /*action_factory*/)
+{
+}
+
+void
+KomodoApp::registerExecFlags(Factory & /*factory*/)
+{
+  /* Uncomment Factory parameter and register your new execution flags here! */
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+KomodoApp__registerApps()
+{
+  KomodoApp::registerApps();
+}
+
+extern "C" void
+KomodoApp__registerObjects(Factory & factory)
+{
+  KomodoApp::registerObjects(factory);
+}
+
+extern "C" void
+KomodoApp__associateSyntax(Syntax & syntax, ActionFactory & action_factory)
+{
+  KomodoApp::associateSyntax(syntax, action_factory);
+}
+
+extern "C" void
+KomodoApp__registerExecFlags(Factory & factory)
+{
+  KomodoApp::registerExecFlags(factory);
+}
